ee5bd623bb6196c09bec0a88b92046629f84342e
Matthew Stefanovic <ms@stefanovic.us>
2024-01-14 17:10:04 -0500

    Added front end for unsubscribe route and table propigates correctly

--- main.py
+++ main.py
@@ -1,6 +1,9 @@
-from flask import Flask, request, redirect, session, render_template
+from flask import Flask, request, redirect, session, render_template, jsonify
 from dotenv import load_dotenv
+from bs4 import BeautifulSoup
 import requests
+import httpx
+import asyncio
 import os
 
 load_dotenv()
@@ -74,21 +77,55 @@ def callback():
         return "Error obtaining access token", 500
 
 
-def get_unsubscribe_link(message_id, folder_id):
-    global account_id, access_token
-
-    message_id = "1704232973332110001"
-    folder_id = "6314644000000008014"
-
+async def get_unsubscribe_link(client, email, account_id, access_token, folder_map):
+    message_id = email['messageId']
+    folder_id = email['folderId']
+    subject = email['subject']
+    sender = email['sender']
+    from_address = email['fromAddress']
     api_url = f'https://mail.zoho.com/api/accounts/{account_id}/folders/{folder_id}/messages/{message_id}/content'
-
     headers = {'Authorization': f'Bearer {access_token}'}
-    response = requests.get(api_url, headers=headers)
+    response = await client.get(api_url, headers=headers)
 
+    unsubscribe_url = None
     if response.status_code == 200:
-        return response.json()
+        data = response.json().get("data", {})
+        content = data.get("content", "").lower()
+
+        if 'unsubscribe' in content:
+            soup = BeautifulSoup(content, 'html.parser')
+            for anchor in soup.find_all('a'):
+                anchor_text = anchor.get_text().lower()
+                anchor_title = anchor.get('title', '').lower()
+                if 'unsubscribe' in anchor_text or 'unsubscribe' in anchor_title or 'here' in anchor_text:
+                    unsubscribe_url = anchor.get('href')
+                    break
+            else:
+                unsubscribe_url = None  # None if no matching URLs found
+        else:
+            return None
     else:
-        return f"Error fetching email message: {response.status_code} {response.text}", 500
+        return f"Error reading email content for {message_id}", 500
+
+    return {
+        "subject": subject,
+        "sender": sender,
+        "from_address": from_address,
+        "content": content,
+        "folder": folder_map.get(folder_id),
+        "unsubscribe_url": unsubscribe_url,
+        "message_id": message_id,
+        "folder_id": folder_id
+    }
+
+
+async def process_emails(account_id, access_token, emails, folder_map):
+    async with httpx.AsyncClient() as client:
+        tasks = [get_unsubscribe_link(
+            client, email, account_id, access_token, folder_map) for email in emails]
+        email_details = await asyncio.gather(*tasks)
+        # Filter out None values
+        return [detail for detail in email_details if detail is not None]
 
 
 @app.route('/unsubscribe')
@@ -97,23 +134,20 @@ def unsubscribe():
 
     # Checks if user is logged in
     if not account_id or not access_token:
-        # Store the URL that initiated the login process
         session['redirect_back_to'] = request.url
         return redirect('/login')
 
     # Gets the folder_id of the inbox
     api_url = f'https://mail.zoho.com/api/accounts/{account_id}/folders'
-
     headers = {'Authorization': f'Bearer {access_token}'}
     folder_response = requests.get(api_url, headers=headers)
 
-    if folder_response.status_code == 200:
-        folder_data = folder_response.json()
-        folder_map = {item["folderId"]: item["path"]
-                      for item in folder_data["data"]}
-
-    else:
+    if folder_response.status_code != 200:
         return f"Error fetching inbox_id: {folder_response.status_code} {folder_response.text}", 500
+
+    folder_data = folder_response.json()
+    folder_map = {item["folderId"]: item["path"]
+                  for item in folder_data["data"]}
 
     # Creates a list of all emails stored in the inbox
     emails = []
@@ -122,33 +156,64 @@ def unsubscribe():
     email_count = 200
 
     while email_count >= 200:
-        # Endpoint to fetch emails
         api_url = f'https://mail.zoho.com/api/accounts/{account_id}/messages/view?start={start}&limit={limit}'
-
         headers = {'Authorization': f'Bearer {access_token}'}
         emails_response = requests.get(api_url, headers=headers)
 
-        if emails_response.status_code == 200:
-            email_data = emails_response.json()['data']
-        else:
+        if emails_response.status_code != 200:
             return f"Error fetching emails: {emails_response.status_code} {emails_response.text}", 500
-        print(f'Getting emails {start}-{start+200}')
 
+        email_data = emails_response.json()['data']
         email_count = len(email_data)
         emails.extend(email_data)
-        start = start + 200
+        start += limit
 
-        # for email in emails:
-        #     message_id = email['messageId']
-        #     folder_id = email['folderId']
-        #     subject = email['subject']
-        #     sender = email['sender']
-        #     from_adderss = email['fromAddress']
-        #     folder = folder_map.get(folder_id)
-        #
-        #     unsubscribe_link = get_unsubscribe_link(message_id, folder_id)
+    # Process all emails asynchronously to get unsubscribe links
+    unsubscribe_links = asyncio.run(process_emails(
+        account_id, access_token, emails, folder_map))
 
-    return emails
+    # Reformat the data into a hashmap
+    email_map = {}
+    for email in unsubscribe_links:
+        from_address = email['from_address']
+        if from_address not in email_map:
+            email_map[from_address] = []
+        email_map[from_address].append(email)
+
+    # Render an HTML template instead of returning JSON
+    return render_template('unsubscribe.html', email_map=email_map)
+    # return emails
+
+
+@app.route('/delete', methods=['POST'])
+def delete():
+    global account_id, access_token
+
+    # Checks if user is logged in
+    if not account_id or not access_token:
+        session['redirect_back_to'] = request.url
+        return redirect('/login')
+
+    # Extract folder_id and message_id from the request
+    folder_id = request.form.get('folder_id')
+    message_id = request.form.get('message_id')
+
+    # Ensure folder_id and message_id are provided
+    if not folder_id or not message_id:
+        return jsonify({'error': 'Missing folder_id or message_id'}), 400
+
+    # The API URL to delete the email
+    api_url = f'https://mail.zoho.com/api/accounts/{account_id}/folders/{folder_id}/messages/{message_id}'
+    headers = {'Authorization': f'Bearer {access_token}'}
+
+    # Send the DELETE request
+    response = requests.delete(api_url, headers=headers)
+
+    # Check if the request was successful
+    if response.status_code == 200:
+        return jsonify({'message': 'Email deleted successfully'}), 200
+    else:
+        return jsonify({'error': 'Failed to delete email', 'status_code': response.status_code}), response.status_code
 
 
 if __name__ == '__main__':
